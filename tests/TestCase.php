7d92b8341a9d1a4d8f339199b0aed32a3830d84b
shajib2021 <codeiteam@gmail.com>
2022-01-02 19:34:35 +0600

    up
    up

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,51 +1,10 @@
 <?php
 
-namespace Billplz\Laravel\Tests;
+namespace Tests;
 
-use Orchestra\Testbench\TestCase as Testbench;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
-abstract class TestCase extends Testbench
+abstract class TestCase extends BaseTestCase
 {
-    /**
-     * Define environment setup.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     */
-    protected function getEnvironmentSetUp($app): void
-    {
-        $app['config']->set('services.billplz', [
-            'key' => '73eb57f0-7d4e-42b9-a544-aeac6e4b0f81',
-            'version' => 'v4',
-            'x-signature' => 'secret',
-            'sandbox' => false,
-        ]);
-    }
-
-    /**
-     * Get package aliases.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     *
-     * @return array
-     */
-    protected function getPackageAliases($app)
-    {
-        return [
-            'Billplz' => \Billplz\Laravel\Billplz::class,
-        ];
-    }
-
-    /**
-     * Get package providers.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     *
-     * @return array
-     */
-    protected function getPackageProviders($app)
-    {
-        return [
-            \Billplz\Laravel\BillplzServiceProvider::class,
-        ];
-    }
+    use CreatesApplication;
 }
